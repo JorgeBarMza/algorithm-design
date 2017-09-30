3830fe835afa5d0ffae49e68b22c0037b1f4b4cc
Jorge Barrios <jorgebarmza@gmail.com>
2017-09-29 23:22:19 -0500

    Solve Ex. 3

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+//suppose words are sorted by length
+vector<string> makeLines(vector<string> words, int linesNum){
+
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
--- 3/main.cpp
+++ 3/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+vector<vector<string>> memo; //100 char limit
+
+string LCS(string w1, string w2){
+    memo.assign(100,vector<string>(100,"-1"));
+    if(memo[w1.size()][w2.size()] == "-1"){
+        if(w1.size() == 0 || w2.size() == 0) return "";
+        if(w1[w1.size()-1] == w2[w2.size()-1]) {
+            memo[w1.size()][w2.size()] = w1[w1.size()-1]+ LCS(w1.erase(w1.size()-1, 1), w2.erase(w2.size()-1,1));
+        }
+        else {
+            string temp1 = w1;
+            string temp2 = w2;
+            temp1.erase(temp1.size()-1,1);
+            temp2.erase(temp2.size()-1,1);
+            string str1 = LCS(temp1, w2);
+            string str2 = LCS(w1,temp2);
+            if(str1.size() >= str2.size()) memo[w1.size()][w2.size()] = str1;
+            memo[w1.size()][w2.size()] = str2;
+        }
+    }
+    return memo[w1.size()][w2.size()];
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    memo.assign(100,vector<string>(100,"-1"));
+    cout << LCS("abcde", "cdefg");
 }
