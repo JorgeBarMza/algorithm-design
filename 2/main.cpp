4f08285b7812b8fd9cf02b79a3439b7fb2253356
Jorge Barrios <jorgebarmza@gmail.com>
2017-10-02 21:19:41 -0500

    Fix Ex. 2 to print justified text and complete Ex. 4

--- 2/main.cpp
+++ 2/main.cpp
@@ -4,16 +4,25 @@
 #include <climits>
 using namespace std;
 int dp[100]; //100 words max
+vector<struct line> lines;
 
-int calcBadness(vector<string>& words, int firstW, int lastW, int lineLenght, int optimalSpaceLength){
+struct line{
+    int startW;
+    int endW;
+    int spaceLength;
+};
+
+int calcBadness(line& curr, vector<string>& words, int firstW, int lastW, int lineLenght, int optimalSpaceLength){
     int remainingLength = lineLenght, individualSpaceLength = 1;
     int wordCount = lastW-firstW+1;
     for (int i = firstW; i <= lastW ; ++i) {
         remainingLength -= (words[i].size() + 1);
     }
     remainingLength++; //last word of line has no space
-    if(0 <= remainingLength && lastW == words.size()-1)
+    if(0 <= remainingLength && lastW == words.size()-1){
+        curr.spaceLength = individualSpaceLength;
         return 0;
+    }
     if(wordCount == 1){
         individualSpaceLength = remainingLength;
         lastW++; // to avoid returning 0
@@ -26,6 +35,7 @@ int calcBadness(vector<string>& words, int firstW, int lastW, int lineLenght, in
             remainingLength -= (wordCount-1);
         }
     }
+    curr.spaceLength = individualSpaceLength;
     return (lastW-firstW) * abs(individualSpaceLength - optimalSpaceLength);
 }
 
@@ -35,21 +45,38 @@ int makeLines(vector<string> words,  int index, int lineLength, int optimalSpace
     if(dp[index] != -1)
         return dp[index];
     int minBadness = INT32_MAX, nextWord;
+    line toAdd;
     for (int i = index; i < words.size(); ++i) {
-        int badness = calcBadness(words, index, i, lineLength, optimalSpaceLength);
-        if(badness <= minBadness){
+        line curr;
+        curr.startW = index;
+        curr.endW = i;
+        int badness = calcBadness(curr, words, index, i, lineLength, optimalSpaceLength);
+        if(badness < minBadness){
             minBadness = badness;
             nextWord = i+1;
+            toAdd = curr;
         }
     }
+    lines.push_back(toAdd);
     return dp[index] = minBadness + makeLines(words, nextWord, lineLength, optimalSpaceLength);
 }
 
 int main() {
     memset(dp, -1, sizeof(dp));
-    vector<string> ws = {"ab", "ab", "aaaaa", "aaaaa"};
-    cout << makeLines(ws, 0, 10, 1) << "\n\n";
-    for (int i = 0; i < 10; ++i) {
-        cout << dp[i] << "\n";
+    vector<string> ws = {"aaa", "aae", "aa", "aaa", "aaa", "aa"};
+    cout << "Cost: " << makeLines(ws, 0, 10, 1) << "\n\n";
+    cout << "Justified Text:\n";
+//    for (int i = 0; i < 10; ++i) {
+//        cout << dp[i] << "\n";
+//    }
+    int currLine = 0;
+    for(auto line:lines){
+        for (int i = line.startW; i < line.endW ; ++i) {
+            cout << ws[i];
+            for (int j = 0; j < line.spaceLength ; ++j) {
+                cout << " ";
+            }
+        }
+        cout << ws[line.endW] << "\n"; // last word no space
     }
 }
---
+++ 4/4/main.cpp
@@ -0,0 +1,68 @@
+//
+//  main.cpp
+//  4
+//
+//  Created by Jorge Barrios on 10/2/17.
+//  Copyright © 2017 Jorge Barrios. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#define n 3
+using namespace std;
+
+vector<vector<int>> men(n,vector<int>(n,-1)), women(n,vector<int>(n,-1));
+
+vector<int>  stableCouples(){
+    vector<int> engaged(n,-1);
+    vector<bool> married(n,false);
+    int singleMen = n;
+    while(singleMen){
+        for(int m = 0; m<n; m++){
+            if(married[m] == true )
+                continue;
+            for (int w = 0; w<n; w++) {
+                int sexyLady = men[m][w];
+                if(engaged[sexyLady] == -1){
+                    engaged[sexyLady] = m;
+                    married[m] = true;
+                    singleMen--;
+                    break;
+                }
+                else{ // check if marriage is unstable
+                    int p = *find(women[sexyLady].begin(), women[sexyLady].end(), engaged[sexyLady]);
+                    int newHusband = *find(women[sexyLady].begin(), women[sexyLady].end(), m);
+                    if(newHusband < p){
+                        engaged[sexyLady] = m;
+                        married[m] = true;
+                        married[engaged[sexyLady]] = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return engaged;
+}
+
+
+
+int main() {
+    
+    for (int i = 0; i<n; i++) {
+        for (int j = 0; j<n; j++) {
+            cin >> men[i][j];
+        }
+    }
+    for (int i = 0; i<n; i++) {
+        for (int j = 0; j<n; j++) {
+            cin >> women[i][j];
+        }
+    }
+    
+    vector<int> res = stableCouples();
+    
+}
+
+
+
